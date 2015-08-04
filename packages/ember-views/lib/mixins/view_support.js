996c020874008a9d94e33c474099569c91c37389
Godhuda <engineering+godhuda@tilde.io>
2015-08-03 17:48:51 -0700

    Fix rebase errors

--- packages/ember-views/lib/mixins/view_support.js
+++ packages/ember-views/lib/mixins/view_support.js
@@ -622,6 +622,7 @@ export default Mixin.create({
     this.scheduledRevalidation = false;
 
     this._super(...arguments);
+    this.renderer.componentInitAttrs(this, this.attrs || {});
   },
 
   __defineNonEnumerable(property) {
@@ -636,24 +637,26 @@ export default Mixin.create({
   scheduleRevalidate(node, label, manualRerender) {
     if (node && !this._dispatching && node.guid in this.env.renderedNodes) {
       if (manualRerender) {
-        Ember.deprecate(`You manually rerendered ${label} (a parent component) from a child component during the rendering process. This rarely worked in Ember 1.x and will be removed in Ember 2.0`);
+        Ember.deprecate(`You manually rerendered ${label} (a parent component) from a child component during the rendering process. This rarely worked in Ember 1.x and will be removed in Ember 2.0`,
+                        false,
+                        { id: 'ember-views.manual-parent-rerender', until: '3.0.0' });
       } else {
-        Ember.deprecate(`You modified ${label} twice in a single render. This was unreliable in Ember 1.x and will be removed in Ember 2.0`);
+        Ember.deprecate(`You modified ${label} twice in a single render. This was unreliable in Ember 1.x and will be removed in Ember 2.0`,
+                        false,
+                        { id: 'ember-views.render-double-modify', until: '3.0.0' });
       }
       run.scheduleOnce('render', this, this.revalidate);
       return;
     }
 
-    Ember.deprecate(`A property of ${this} was modified inside the ${this._dispatching} hook. You should never change properties on components, services or models during ${this._dispatching} because it causes significant performance degradation.`, !this._dispatching);
+    Ember.deprecate(`A property of ${this} was modified inside the ${this._dispatching} hook. You should never change properties on components, services or models during ${this._dispatching} because it causes significant performance degradation.`,
+                    !this._dispatching,
+                    { id: 'ember-views.dispatching-modify-property', until: '3.0.0' });
 
     if (!this.scheduledRevalidation || this._dispatching) {
       this.scheduledRevalidation = true;
       run.scheduleOnce('render', this, this.revalidate);
     }
-  },
-
-  appendAttr(node, buffer) {
-    return this.currentState.appendAttr(this, node, buffer);
   },
 
   templateRenderer: null,
